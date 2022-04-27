0b98459c85c8ef43a72e9af58b48e2b97ac27051
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-04-27 14:51:22 +0100

    45th solution to twice linear problem

--- src/main/scala/Twice_Linear.scala
+++ src/main/scala/Twice_Linear.scala
@@ -7,33 +7,53 @@ object Twice_Linear extends App {
     var y = 0
     var z = 0
 
-    while (u.size <= 2*n) {
-      y = 2 * u(pointer) + 1
-      z = 3 * u(pointer) + 1
-      if (!u.contains(y)) {
-        u = u :+ y
-      }
-      if (!u.contains(z)) {
-        u = u :+ z
-      }
-      u = u.sorted
+    //
 
-      pointer += 1
+    //    while (u.size <= 2*n) {
+    //      y = 2 * u(pointer) + 1
+    //      z = 3 * u(pointer) + 1
+    //      if (!u.contains(y)) {
+    //        u = u :+ y
+    //      }
+    //      if (!u.contains(z)) {
+    //        u = u :+ z
+    //      }
+    ////      u = u.sorted
+    //
+    //      pointer += 1
+    //    }
+
+    var z_index = 0
+    var y_index = 0
+    while (u.size <= n) {
+      y = 2 * u(y_index) + 1
+      z = 3 * u(z_index) + 1
+      if (y > z) {
+
+        u = u :+ z
+        z_index += 1
+      } else if (y == z) {
+        u = u :+ y
+        z_index += 1
+        y_index += 1
+      }
+      else {
+        u = u :+ y
+        y_index += 1
+      }
+
+      //      if (!u.contains(y)) {
+      //        u = u :+ y
+      //      }
+      //      if (!u.contains(z)) {
+      //        u = u :+ z
+      //      }
+      //      u = u.sorted
     }
-    println(u.length)
-    println(u)
+//    println(u.length)
+//    println(u)
     u(n)
   }
-  println(dblLinear(10))
+
+  println(dblLinear(60000))
 }
-//    for (index <- 0 to n - 1) yield {
-//      y = 2 * u(index) + 1
-//      z = 3 * u(index) + 1
-//      if (!u.contains(y)) {
-//        u = u :+ y
-//      }
-//      if (!u.contains(z)) {
-//        u = u :+ z
-//      }
-//      u = u.sorted
-//    }
