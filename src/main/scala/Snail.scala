dab428e537cb4106f92160a675f017f7d626e16c
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-04-28 07:46:43 +0100

    pseudo-coding Snail problem

---
+++ src/main/scala/Snail.scala
@@ -0,0 +1,8 @@
+object Snail extends App {
+  def snail(xs: List[List[Int]]): List[Int] = ???
+
+  // create 2d array using input
+  // use pointer to go right, down, left, up - fill resultArray as you go
+  // then use recursion to repeat
+  // stop when no numbers left
+}
