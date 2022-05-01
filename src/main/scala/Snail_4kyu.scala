ba8faa96379eb2d5f2e332f62d7d375815a022b8
Darshil Dholakia <darshildholakia@Darshils-MacBook-Air.local>
2022-05-01 09:50:18 +0100

    repeating 9 bug fixed

--- src/main/scala/Snail_4kyu.scala
+++ src/main/scala/Snail_4kyu.scala
@@ -12,31 +12,30 @@ object Snail_4kyu extends App {
 //    val test2 = List(List(1,2),List(3,4),List(5,6))
 //    println(test2(2)(1))
 
-//    recursion(0)
-
-//    def recursion:(Int => Unit) = (rowIndex: Int) => {
-
-//      val upFunc = (rowIndex: Int) => {
-//        var upRowIndex = rowIndex
-//        var row = input(upRowIndex)
-//        while (upRowIndex > input.length) {
-//          row = input(upRowIndex)
-//          resultList = resultList :+ row(0)
-//          row = row.patch(0, Nil, 1)
-//          upRowIndex -= 1
-//        }
-//        recursion(upRowIndex + 1)
-//      }
+      val upFunc = (rowIndex: Int) => {
+        var upRowIndex = rowIndex
+        var row = input(upRowIndex)
+        while (upRowIndex > input.length) {
+          row = input(upRowIndex)
+          resultList = resultList :+ row(0)
+          row = row.patch(0, Nil, 1)
+          upRowIndex -= 1
+        }
+        rightFunc(upRowIndex+1)
+      }
 
       val leftFunc = (rowIndex: Int) => {
         var leftRowIndex = rowIndex
         var row = input(leftRowIndex)
+        row = row.patch(row.length - 1, Nil, 1) // since this element has already been accounted for by downFunc()
+        println(row)
         while (row.length > 0) {
           resultList = resultList :+ row(row.length - 1)
           row = row.patch(row.length - 1, Nil, 1) // remove 1 element at index = right_index within row of input with number row_index
+          println(row)
         }
         leftRowIndex -= 1
-//        upFunc(leftRowIndex)
+        upFunc(leftRowIndex)
       }
 
       val downFunc = (rowIndex: Int) => {
@@ -47,18 +46,10 @@ object Snail_4kyu extends App {
           resultList = resultList :+ row(row.length - 1)
           row = row.patch(row.length - 1, Nil, 1)
           downRowIndex += 1
+//          println(row)
         }
         leftFunc(downRowIndex - 1)
       }
-
-//      var rightRowIndex = rowIndex
-//      var row = input(rightRowIndex)
-//      while (row.length > 0) {
-//        resultList = resultList :+ row(0)
-//        row = row.patch(0, Nil, 1) // remove 1 element at index = right_index within row of input with number row_index
-//      }
-//      rightRowIndex += 1
-//      downFunc(rightRowIndex)
 
       val rightFunc = (rowIndex: Int) => {
         var rightRowIndex = rowIndex
@@ -71,7 +62,6 @@ object Snail_4kyu extends App {
         downFunc(rightRowIndex)
       }
 
-//    }
     rightFunc(0)
     println(input)
     resultList
